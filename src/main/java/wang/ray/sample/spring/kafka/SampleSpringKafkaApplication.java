8f82233d063e9a9f2bb00f7d6eddc12da0ffefdb
Ray WANG <2532542@qq.com>
2019-03-22 09:34:29 +0800

    init

---
+++ src/main/java/wang/ray/sample/spring/kafka/SampleSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package wang.ray.sample.spring.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringKafkaApplication.class, args);
+    }
+
+}
