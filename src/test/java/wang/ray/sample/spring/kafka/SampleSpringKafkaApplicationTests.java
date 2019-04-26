4c65e6926d26f04533403531e839182bbc0c67bb
Ray WANG <2532542@qq.com>
2019-04-26 17:17:45 +0800

    添加泛型

--- src/test/java/wang/ray/sample/spring/kafka/SampleSpringKafkaApplicationTests.java
+++ src/test/java/wang/ray/sample/spring/kafka/SampleSpringKafkaApplicationTests.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SampleSpringKafkaApplicationTests {
 
     @Autowired
-    private KafkaTemplate kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @Test
     public void testDemo() throws Exception {
